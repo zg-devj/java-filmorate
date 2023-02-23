d5f6331bdcb913efb519f6f5777768ef1b747134
Gleb Zakharov <webmaster-80@yandex.ru>
2023-02-23 09:42:07 +0300

    feat: добавить файлы исключений

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
+// для кода 400
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
