494ea651ca45d641cef166c9fad8ee6f88ae6ab3
Gleb Zakharov <webmaster-80@yandex.ru>
2023-02-23 09:43:42 +0300

    feat: добавить классы ответа сервера

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/MessageResponse.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmorate.model;
+
+public class MessageResponse {
+    private String message;
+
+    public MessageResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
