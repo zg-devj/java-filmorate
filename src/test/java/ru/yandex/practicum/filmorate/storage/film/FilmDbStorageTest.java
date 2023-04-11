9434576cbcc03a28b658d5ae65ca0a53f3bc986a
Gleb Zakharov <webmaster-80@yandex.ru>
2023-04-11 10:19:46 +0300

    fix: подправить тесты 2

--- src/test/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorageTest.java
@@ -3,7 +3,7 @@ package ru.yandex.practicum.filmorate.storage.film;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -27,7 +27,7 @@ import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchException;
 
-@SpringBootApplication
+@SpringBootTest
 class FilmDbStorageTest {
     private EmbeddedDatabase embeddedDatabase;
     private JdbcTemplate jdbcTemplate;
