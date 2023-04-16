c40c2e681d9fe812e30c09ec13343ff43fbdad92
molestov <alexander@molestov.ru>
2023-04-16 13:27:48 +0500

    add-feed

--- src/main/java/ru/yandex/practicum/filmorate/storage/event/EventStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/event/EventStorage.java
@@ -5,9 +5,21 @@ import ru.yandex.practicum.filmorate.model.Event;
 import java.util.List;
 
 public interface EventStorage {
-    enum TypeName {LIKE, REVIEW, FRIEND}
-    enum OperationName {REMOVE, ADD, UPDATE}
+    enum TypeName {
+        LIKE,
+        REVIEW,
+        FRIEND
+    }
+
+    enum OperationName {
+        REMOVE,
+        ADD,
+        UPDATE
+    }
+
     List<Event> getEventsByUserId(Long id);
+
     void addEvent(Long userId, Long entityId, TypeName type, OperationName operation);
+
     void removeEventsByUserId(Long id);
 }
